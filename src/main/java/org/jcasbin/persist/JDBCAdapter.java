cc33de5b592db91493444652d35f960b140439b1
Yang Luo <hsluoyz@qq.com>
2018-08-03 15:34:45 +0800

    Add the code.

---
+++ src/main/java/org/jcasbin/persist/JDBCAdapter.java
@@ -0,0 +1,279 @@
+// Copyright 2018 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.jcasbin.persist;
+
+import org.casbin.jcasbin.model.Assertion;
+import org.casbin.jcasbin.model.Model;
+import org.casbin.jcasbin.persist.Adapter;
+import org.casbin.jcasbin.persist.Helper;
+
+import java.sql.*;
+import java.util.List;
+import java.util.Map;
+
+class CasbinRule {
+    String ptype;
+    String v0;
+    String v1;
+    String v2;
+    String v3;
+    String v4;
+    String v5;
+}
+
+/**
+ * JDBCAdapter is the JDBC adapter for jCasbin.
+ * It can load policy from JDBC supported database or save policy to it.
+ */
+public class JDBCAdapter implements Adapter {
+    private String driver;
+    private String url;
+    private String username;
+    private String password;
+
+    private Connection conn = null;
+
+    /**
+     * JDBCAdapter is the constructor for JDBCAdapter.
+     *
+     * @param driver the path of the policy file.
+     * @param url the path of the policy file.
+     * @param username the path of the policy file.
+     * @param password the path of the policy file.
+     */
+    public JDBCAdapter(String driver, String url, String username, String password) {
+        this.driver = driver;
+        this.url = url;
+        this.username = username;
+        this.password = password;
+
+        open();
+    }
+
+    public void finalize() {
+        close();
+    }
+
+    private void open() {
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void close() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        conn = null;
+    }
+
+    private void createTable() {
+        try {
+            Statement stmt = conn.createStatement();
+            String sql = "CREATE TABLE casbin_rule " +
+                    "(ptype VARCHAR(100) not NULL, " +
+                    " v0 VARCHAR(100), " +
+                    " v1 VARCHAR(100), " +
+                    " v2 VARCHAR(100), " +
+                    " v3 VARCHAR(100), " +
+                    " v4 VARCHAR(100), " +
+                    " v5 VARCHAR(100))";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void dropTable() {
+        try {
+            Statement stmt = conn.createStatement();
+            String sql = "DROP TABLE IF EXISTS casbin_rule";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadPolicyLine(CasbinRule line, Model model) {
+        String lineText = line.ptype;
+        if (!line.v0.equals("")) {
+            lineText += ", " + line.v0;
+        }
+        if (!line.v1.equals("")) {
+            lineText += ", " + line.v1;
+        }
+        if (!line.v2.equals("")) {
+            lineText += ", " + line.v2;
+        }
+        if (!line.v3.equals("")) {
+            lineText += ", " + line.v3;
+        }
+        if (!line.v4.equals("")) {
+            lineText += ", " + line.v4;
+        }
+        if (!line.v5.equals("")) {
+            lineText += ", " + line.v5;
+        }
+
+        Helper.loadPolicyLine(lineText, model);
+    }
+
+    /**
+     * loadPolicy loads all policy rules from the storage.
+     */
+    @Override
+    public void loadPolicy(Model model) {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rSet = stmt.executeQuery("SELECT * FROM casbin_rule");
+            ResultSetMetaData rData = rSet.getMetaData();
+            while (rSet.next()) {
+                CasbinRule line = new CasbinRule();
+                for (int i = 1; i <= rData.getColumnCount(); i++) {
+                    if (i == 1) {
+                        line.ptype = rSet.getObject(i) == null ? "" : (String) rSet.getObject(i);
+                    } else if (i == 2) {
+                        line.v0 = rSet.getObject(i) == null ? "" : (String) rSet.getObject(i);
+                    } else if (i == 3) {
+                        line.v1 = rSet.getObject(i) == null ? "" : (String) rSet.getObject(i);
+                    } else if (i == 4) {
+                        line.v2 = rSet.getObject(i) == null ? "" : (String) rSet.getObject(i);
+                    } else if (i == 5) {
+                        line.v3 = rSet.getObject(i) == null ? "" : (String) rSet.getObject(i);
+                    } else if (i == 6) {
+                        line.v4 = rSet.getObject(i) == null ? "" : (String) rSet.getObject(i);
+                    } else if (i == 7) {
+                        line.v5 = rSet.getObject(i) == null ? "" : (String) rSet.getObject(i);
+                    }
+                }
+                loadPolicyLine(line, model);
+            }
+            rSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private CasbinRule savePolicyLine(String ptype, List<String> rule) {
+        CasbinRule line = new CasbinRule();
+
+        line.ptype = ptype;
+        if (rule.size() > 0) {
+            line.v0 = rule.get(0);
+        }
+        if (rule.size() > 1) {
+            line.v1 = rule.get(1);
+        }
+        if (rule.size() > 2) {
+            line.v2 = rule.get(2);
+        }
+        if (rule.size() > 3) {
+            line.v3 = rule.get(3);
+        }
+        if (rule.size() > 4) {
+            line.v4 = rule.get(4);
+        }
+        if (rule.size() > 5) {
+            line.v5 = rule.get(5);
+        }
+
+        return line;
+    }
+
+    /**
+     * savePolicy saves all policy rules to the storage.
+     */
+    @Override
+    public void savePolicy(Model model) {
+        dropTable();
+        createTable();
+
+        String sql = "INSERT INTO casbin_rule (ptype,v0,v1,v2,v3,v4,v5) VALUES(?,?,?,?,?,?,?)";
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement(sql);
+
+
+        for (Map.Entry<String, Assertion> entry : model.model.get("p").entrySet()) {
+            String ptype = entry.getKey();
+            Assertion ast = entry.getValue();
+
+            for (List<String> rule : ast.policy) {
+                CasbinRule line = savePolicyLine(ptype, rule);
+
+                ps.setString(1, line.ptype);
+                ps.setString(2, line.v0);
+                ps.setString(3, line.v1);
+                ps.setString(4, line.v2);
+                ps.setString(5, line.v3);
+                ps.setString(6, line.v4);
+                ps.setString(7, line.v5);
+                ps.addBatch();
+            }
+        }
+
+        for (Map.Entry<String, Assertion> entry : model.model.get("g").entrySet()) {
+            String ptype = entry.getKey();
+            Assertion ast = entry.getValue();
+
+            for (List<String> rule : ast.policy) {
+                CasbinRule line = savePolicyLine(ptype, rule);
+
+                ps.setString(1, line.ptype);
+                ps.setString(2, line.v0);
+                ps.setString(3, line.v1);
+                ps.setString(4, line.v2);
+                ps.setString(5, line.v3);
+                ps.setString(6, line.v4);
+                ps.setString(7, line.v5);
+                ps.addBatch();
+            }
+        }
+
+        ps.executeBatch();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * addPolicy adds a policy rule to the storage.
+     */
+    @Override
+    public void addPolicy(String sec, String ptype, List<String> rule) {
+        throw new Error("not implemented");
+    }
+
+    /**
+     * removePolicy removes a policy rule from the storage.
+     */
+    @Override
+    public void removePolicy(String sec, String ptype, List<String> rule) {
+        throw new Error("not implemented");
+    }
+
+    /**
+     * removeFilteredPolicy removes policy rules that match the filter from the storage.
+     */
+    @Override
+    public void removeFilteredPolicy(String sec, String ptype, int fieldIndex, String... fieldValues) {
+        throw new Error("not implemented");
+    }
+}
---
+++ src/test/java/org/jcasbin/persist/JDBCAdapterTest.java
@@ -0,0 +1,74 @@
+// Copyright 2018 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.jcasbin.persist;
+
+import org.casbin.jcasbin.main.Enforcer;
+import org.casbin.jcasbin.util.Util;
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.fail;
+
+public class JDBCAdapterTest {
+    private static void testGetPolicy(Enforcer e, List<List<String>> res) {
+        List<List<String>> myRes = e.getPolicy();
+        Util.logPrint("Policy: " + myRes);
+
+        if (!Util.array2DEquals(res, myRes)) {
+            fail("Policy: " + myRes + ", supposed to be " + res);
+        }
+    }
+
+    @Test
+    public void testAdapter() {
+        // Because the DB is empty at first,
+        // so we need to load the policy from the file adapter (.CSV) first.
+        Enforcer e = new Enforcer("examples/rbac_model.conf", "examples/rbac_policy.csv");
+
+        JDBCAdapter a = new JDBCAdapter("com.mysql.cj.jdbc.Driver", "jdbc:mysql://localhost:3306/casbin_rule", "root", "1234");
+        // This is a trick to save the current policy to the DB.
+        // We can't call e.savePolicy() because the adapter in the enforcer is still the file adapter.
+        // The current policy means the policy in the jCasbin enforcer (aka in memory).
+        a.savePolicy(e.getModel());
+
+        // Clear the current policy.
+        e.clearPolicy();
+        testGetPolicy(e, asList());
+
+        // Load the policy from DB.
+        a.loadPolicy(e.getModel());
+        testGetPolicy(e, asList(
+                asList("alice", "data1", "read"),
+                asList("bob", "data2", "write"),
+                asList("data2_admin", "data2", "read"),
+                asList("data2_admin", "data2", "write")));
+
+        // Note: you don't need to look at the above code
+        // if you already have a working DB with policy inside.
+
+        // Now the DB has policy, so we can provide a normal use case.
+        // Create an adapter and an enforcer.
+        // new Enforcer() will load the policy automatically.
+        a = new JDBCAdapter("com.mysql.cj.jdbc.Driver", "jdbc:mysql://localhost:3306/casbin_rule", "root", "1234");
+        e = new Enforcer("examples/rbac_model.conf", a);
+        testGetPolicy(e, asList(
+                asList("alice", "data1", "read"),
+                asList("bob", "data2", "write"),
+                asList("data2_admin", "data2", "read"),
+                asList("data2_admin", "data2", "write")));
+    }
+}
